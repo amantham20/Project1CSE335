c44da9d7bd34230e9ce461139beb6d8f01d4f6de
Ian Valdovinos <valdovi7@msu.edu>
2022-10-14 17:33:58 -0400

    Make ScoreView::SetScore() private and add a ScoreView::ClearScore() method

--- SpartyLib/ScoreView.h
+++ SpartyLib/ScoreView.h
@@ -16,6 +16,12 @@ private:
     /// Current score
     int mScore = 0;
 
+    /**
+     * Sets the score to a specified value
+     * @param newScore New score to set the ScoreView to
+     */
+    void SetScore(int newScore) {mScore = newScore; }
+
 public:
     /**
      * Gets the current score
@@ -24,16 +30,15 @@ public:
     int GetScore() {return mScore;}
 
     /**
-     * Sets the score to a specified value
-     * @param newScore New score to set the ScoreView to
-     */
-    void SetScore(int newScore) {mScore = newScore; }
-
-    /**
      * Adds to the current score
      * @param score Score to add to the ScoreView
      */
     void AddScore(int score) {mScore += score; }
+
+    /**
+     * Clears the score board
+     */
+    void ClearScore() { mScore = 0; }
 };
 
 #endif //ANGRYSPARTY_SCOREVIEW_H
