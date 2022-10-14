d6375170d8ef182f50a65865d9d2fe93855547a9
Ian Valdovinos <valdovi7@msu.edu>
2022-10-14 09:45:44 -0400

    implement ScoreView class

---
+++ SpartyLib/ScoreView.cpp
@@ -0,0 +1,7 @@
+/**
+ * @file ScoreView.cpp
+ * @author Ian Valdovinos Granados
+ */
+
+#include "pch.h"
+#include "ScoreView.h"
---
+++ SpartyLib/ScoreView.h
@@ -0,0 +1,39 @@
+/**
+ * @file ScoreView.h
+ * @author Ian Valdovinos Granados
+ *
+ * Class that keeps track of the current score for the game or a specific level
+ */
+
+#ifndef ANGRYSPARTY_SCOREVIEW_H
+#define ANGRYSPARTY_SCOREVIEW_H
+
+/**
+ * Class that keeps track of the current score for the game or a specific level
+ */
+class ScoreView {
+private:
+    /// Current score
+    int mScore = 0;
+
+public:
+    /**
+     * Gets the current score
+     * @return Current score
+     */
+    int GetScore() {return mScore;}
+
+    /**
+     * Sets the score to a specified value
+     * @param newScore New score to set the ScoreView to
+     */
+    void SetScore(int newScore) {mScore = newScore; }
+
+    /**
+     * Adds to the current score
+     * @param score Score to add to the ScoreView
+     */
+    void AddScore(int score) {mScore += score; }
+};
+
+#endif //ANGRYSPARTY_SCOREVIEW_H
