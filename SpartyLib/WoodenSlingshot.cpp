54665608b7814bcc0618f59f5ed8b1177ac86334
Milan Mihailovic <mihailo8@msu.edu>
2022-10-16 13:10:33 -0400

    Added Inherited Slingshot class "WoodenSlingshot"
    Added Inherited Slingshot class "WoodenSlingshot", which will be the class where I will be doing most of my work in for now.

---
+++ SpartyLib/WoodenSlingshot.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file WoodenSlingshot.cpp
+ * @author Milan Mihailovic
+ */
+
+#include "pch.h"
+#include "WoodenSlingshot.h"
+
+using namespace std;
---
+++ SpartyLib/WoodenSlingshot.h
@@ -0,0 +1,23 @@
+/**
+ * @file WoodenSlingshot.h
+ * @author Milan Mihailovic
+ *
+ * Class for a slingshot of type WoodenSlingshot
+ */
+
+#ifndef ANGRYSPARTY_WOODENSLINGSHOT_H
+#define ANGRYSPARTY_WOODENSLINGSHOT_H
+
+#include "Slingshot.h"
+
+/**
+ * Class for a slingshot of type WoodenSlingshot
+ */
+class WoodenSlingshot : public Slingshot {
+private:
+
+public:
+
+};
+
+#endif //ANGRYSPARTY_WOODENSLINGSHOT_H
