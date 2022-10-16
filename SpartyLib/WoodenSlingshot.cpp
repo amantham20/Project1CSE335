cf1d71fab9c391072f0e59e0c4991020017e1f47
Milan Mihailovic <mihailo8@msu.edu>
2022-10-16 13:22:41 -0400

    Added useful constants to the start of WoodenSlingshot.cpp
    I commented out the b2Vec2 ones because we have not implemented Box2D yet

--- SpartyLib/WoodenSlingshot.cpp
+++ SpartyLib/WoodenSlingshot.cpp
@@ -7,3 +7,30 @@
 #include "WoodenSlingshot.h"
 
 using namespace std;
+/// Base filename for the slingshot image
+const std::wstring WoodSlingshotBaseName = L"slingshot";
+
+/// Size of the slingshot image in meters
+//const b2Vec2 WoodSlingshotSize = b2Vec2(0.5, 1.446);
+
+/// Back band attachment point
+//const b2Vec2 WoodSlingshotBandAttachBack = b2Vec2(-0.15f, 1.12f);
+
+/// Front band attachment point
+//const b2Vec2 WoodSlingshotBandAttachFront = b2Vec2(0.15f, 1.2f);
+
+/// Maximum amount the slingshot can be pulled in meters
+const double WoodSlingshotMaximumPull = 1;
+
+/// Pull angles from -pi to this value are allowed
+const double SlingshotMaximumNegativePullAngle = -1.7;
+
+/// Pull angles from +pi to this value are allowed
+const double SlingshotMinimumPositivePullAngle = 2.42;
+
+/// Width of the slingshot band in centimeters
+const int SlingshotBandWidth = 15;
+
+/// The slingshot band colour
+const wxColour SlingshotBandColor = wxColour(55, 18, 1);
+
