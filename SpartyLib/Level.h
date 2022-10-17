7583fb10a2040191e0cea6735ee5cb4cfff2cd7f
Ian Valdovinos <valdovi7@msu.edu>
2022-10-17 17:35:06 -0400

    add mItems vector to the Level class to keep track of the item in a certain Level

--- SpartyLib/Level.h
+++ SpartyLib/Level.h
@@ -8,12 +8,17 @@
 #ifndef ANGRYSPARTY_LEVEL_H
 #define ANGRYSPARTY_LEVEL_H
 
+#include "Item.h"
+
 class Level {
 private:
     double mHeight = 0;
     double mWidth = 0;
 
     int mLevelScore = 0;
+
+    /// All of the items in the level
+    std::vector<std::shared_ptr<Item>> mItems;
 
 public:
 
