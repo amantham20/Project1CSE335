31477f2a72a2bb532da73f6f32f67d15119efcff
Rajmeet Singh <chandok1@msu.edu>
2022-10-17 09:50:24 -0400

    Fixed merge conflicts

---
+++ SpartyLib/PositionalItem.cpp
@@ -0,0 +1,6 @@
+/**
+ * @file PositionalItem.cpp
+ * @author Rajmeet Singh Chandok
+ */
+
+#include "PositionalItem.h"
---
+++ SpartyLib/PositionalItem.h
@@ -0,0 +1,27 @@
+/**
+ * @file PositionalItem.h
+ * @author Rajmeet Singh Chandok
+ *
+ *
+ */
+
+#ifndef ANGRYSPARTY_POSITIONALITEM_H
+#define ANGRYSPARTY_POSITIONALITEM_H
+
+#include "Item.h"
+
+class PositionalItem : public Item {
+private:
+    /// x position
+    double mX;
+
+    /// y position
+    double mY;
+
+    ///
+
+public:
+
+};
+
+#endif //ANGRYSPARTY_POSITIONALITEM_H
