2da42bf86229ea1775e33cbf0480ce0b5715ad67
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2022-10-17 09:19:01 -0400

    Initialized Level.cpp and Level.h classes

---
+++ SpartyLib/Level.h
@@ -0,0 +1,23 @@
+/**
+ * @file Level.h
+ * @author Aman Dhruva Thamminana
+ *
+ *
+ */
+
+#ifndef ANGRYSPARTY_LEVEL_H
+#define ANGRYSPARTY_LEVEL_H
+
+class Level {
+private:
+    double mHeight = 0;
+    double mWidth = 0;
+
+    int mLevelScore = 0;
+
+public:
+
+
+};
+
+#endif //ANGRYSPARTY_LEVEL_H
