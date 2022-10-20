525e69f5c9c68ac439029297e3ac2f2fed7c8b0e
Milan Mihailovic <mihailo8@msu.edu>
2022-10-20 16:17:40 -0400

    Added a BodyItem inherited class from PositionalItem (refer to updated class diagram)

---
+++ SpartyLib/BodyItem.cpp
@@ -0,0 +1,7 @@
+/**
+ * @file BodyItem.cpp
+ * @author Milan Mihailovic
+ */
+
+#include "pch.h"
+#include "BodyItem.h"
---
+++ SpartyLib/BodyItem.h
@@ -0,0 +1,20 @@
+/**
+ * @file BodyItem.h
+ * @author Milan Mihailovic
+ *
+ *
+ */
+
+#ifndef ANGRYSPARTY_BODYITEM_H
+#define ANGRYSPARTY_BODYITEM_H
+
+#include "PositionalItem.h"
+
+class BodyItem : public PositionalItem {
+private:
+
+public:
+
+};
+
+#endif //ANGRYSPARTY_BODYITEM_H
