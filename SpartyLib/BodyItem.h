d99260a70169d5f31479c30eaeb13b07bc2ce422
Milan Mihailovic <mihailo8@msu.edu>
2022-10-20 16:59:37 -0400

    Added body to BodyItem class

--- SpartyLib/BodyItem.h
+++ SpartyLib/BodyItem.h
@@ -9,11 +9,16 @@
 #define ANGRYSPARTY_BODYITEM_H
 
 #include "PositionalItem.h"
+#include <box2d.h>
 
 class BodyItem : public PositionalItem {
 private:
+    /// box2d Item Body
+    b2Body* mBody;
 
 public:
+
+    BodyItem(b2World* world);
 
 };
 
