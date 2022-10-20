7490c2cb51324ab8938ebf1d6fe297d3f20b4938
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2022-10-19 20:31:51 -0400

    Update Background.h
    Added a constructor

--- SpartyLib/Background.h
+++ SpartyLib/Background.h
@@ -8,10 +8,17 @@
 #ifndef ANGRYSPARTY_BACKGROUND_H
 #define ANGRYSPARTY_BACKGROUND_H
 
-class Background {
-private:
+#include "Item.h"
 
+class Background : public Item{
+private:
+//    std::unique_ptr<wxBitmap> mBackground;
 public:
+//    Background(std::unique_ptr<wxBitmap> pBackground):Item()
+
+    Background(const std::wstring& filename) : Item(filename){};
+
+
 
 };
 
