d18968df290977f0c54d91ee8a75f53ea861da87
Milan Mihailovic <mihailo8@msu.edu>
2022-10-21 19:17:05 -0400

    Made slingshot work :+1:

--- SpartyLib/WoodenSlingshot.cpp
+++ SpartyLib/WoodenSlingshot.cpp
@@ -6,8 +6,12 @@
 #include "pch.h"
 #include "WoodenSlingshot.h"
 #include "Consts.h"
+#include "Item.h"
 
 using namespace std;
+
+/// Slingshot filename
+const wstring WoodenSlingshotImageName = L"images/slingshot.png";
 
 /**
  * WoodenSlingshot constructor
@@ -15,25 +19,26 @@ using namespace std;
  */
 WoodenSlingshot::WoodenSlingshot(std::shared_ptr<Level> level) : Slingshot(level)
 {
+    Item::SetImageName(WoodenSlingshotImageName);
 }
 
-void WoodenSlingshot::OnDraw(std::shared_ptr<wxGraphicsContext> graphics){
+void WoodenSlingshot::OnDraw(std::shared_ptr<wxGraphicsContext> graphics)
+{
+    const int heightOffset = 50;
 
-    graphics->PushState();
+    auto tLevel = Item::GetLevel();
+    auto wid = tLevel->GetWidth() * Consts::MtoCM;
+    auto hit = tLevel->GetHeight()  * Consts::MtoCM;
 
     auto position = PositionalItem::GetPosition();
+    auto image = Item::GetBitMap();
 
-    graphics->Translate(position.x * Consts::MtoCM,
-            position.y * Consts::MtoCM);
-
-    std::shared_ptr<wxBitmap> bitmap = Item::GetBitMap();
-
+    graphics->PushState();
     graphics->Scale(1, -1);
-
-    graphics->DrawBitmap(*bitmap,
+    graphics->DrawBitmap(*Item::GetBitMap(),
             position.x * Consts::MtoCM,
-            position.y * Consts::MtoCM,
-            50 * Consts::MtoCM, 50 * Consts::MtoCM);
+            (position.y  * Consts::MtoCM) - image->GetHeight() - heightOffset,
+            image->GetWidth(), image->GetHeight());
 
     graphics->PopState();
 }
