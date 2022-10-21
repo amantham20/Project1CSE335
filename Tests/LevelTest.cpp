2f10f5bb9fc85b5d98a3d9243a0428e73937b79e
Ian Valdovinos <valdovi7@msu.edu>
2022-10-21 15:14:21 -0400

    Create LevelTest class and TempPath() function.

--- Tests/LevelTest.cpp
+++ Tests/LevelTest.cpp
@@ -5,3 +5,24 @@
 
 #include <pch.h>
 #include "gtest/gtest.h"
+#include <wx/filename.h>
+
+class LevelTest : public ::testing::Test
+{
+protected:
+    /**
+    * Create a path to a place to put temporary files
+    */
+    wxString TempPath()
+    {
+        // Create a temporary filename we can use
+        auto path = wxFileName::GetTempDir() + L"/aquarium";
+        if(!wxFileName::DirExists(path))
+        {
+            wxFileName::Mkdir(path);
+        }
+
+        return path;
+    }
+
+};
