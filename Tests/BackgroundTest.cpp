fbd8134dd524655e738c6713239abaeb5a0c1dfd
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2022-10-21 19:18:42 -0400

    Added Background Tests

---
+++ Tests/BackgroundTest.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file BackgroundTest.cpp
+ * @author Aman Dhruva Thamminana
+ */
+
+
+#include <pch.h>
+#include "gtest/gtest.h"
+#include <Score.h>
+#include <Block.h>
+#include <wx/filefn.h>
+
+
