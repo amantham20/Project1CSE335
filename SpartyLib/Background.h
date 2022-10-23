2eb8f0c2338bef87675321f7040479bdb584e823
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2022-10-23 00:52:26 -0400

    Added Documentation to Background.h

--- SpartyLib/Background.h
+++ SpartyLib/Background.h
@@ -10,12 +10,28 @@
 
 #include "Item.h"
 
+
+/**
+ * Background Class
+ *
+ * @author Aman Dhruva Thamminana
+ */
 class Background : public Item{
 private:
 
 public:
 
     void XmlLoad(wxXmlNode *node) override;
+
+    /**
+     * Constructor for the Background image
+     *
+     * Calls Item and passes in Level
+     *
+     * @param level The current Level the Object is in.
+     *
+     * @author Aman Dhruva Thamminana
+     */
     Background(std::shared_ptr<Level> level) :Item(level){};
     void OnDraw(std::shared_ptr<wxGraphicsContext> graphics) override;
 
