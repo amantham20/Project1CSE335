4df866712d5476e6b87614c490bbd91ef4b9e31d
James <nizhiqia@msu.edu>
2022-10-25 13:37:48 -0400

    Empty angry class

---
+++ SpartyLib/Angry.cpp
@@ -0,0 +1,5 @@
+//
+// Created by James on 10/25/22.
+//
+
+#include "Angry.h"
---
+++ SpartyLib/Angry.h
@@ -0,0 +1,18 @@
+//
+// Created by James on 10/25/22.
+//
+
+#ifndef ANGRYSPARTY_ANGRY_H
+#define ANGRYSPARTY_ANGRY_H
+
+
+#include "BodyItem.h"
+
+class Angry : public BodyItem {
+private:
+public:
+
+};
+
+
+#endif //ANGRYSPARTY_ANGRY_H
