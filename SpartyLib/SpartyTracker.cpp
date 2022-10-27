712df763c335a5c71cb27c4082e1d7947aa2fd54
Ian Valdovinos <valdovi7@msu.edu>
2022-10-27 19:27:26 -0400

    Implement SpartyTracker::VisitHelmetSparty and SpartyTracker::VisitGruffSparty functions.

--- SpartyLib/SpartyTracker.cpp
+++ SpartyLib/SpartyTracker.cpp
@@ -5,3 +5,23 @@
 
 #include "pch.h"
 #include "SpartyTracker.h"
+
+/**
+ * Visit a HelmetSparty object
+ * @param helmetSparty Angry Sparty we are visiting
+ */
+void SpartyTracker::VisitHelmetSparty(HelmetSparty* helmetSparty)
+{
+    mSparties.push_back(helmetSparty);
+    mNumSparty++;
+}
+
+/**
+ * Visit a GruffSpartu object
+ * @param gruffSparty Angry Spartu we are visiting
+ */
+void SpartyTracker::VisitGruffSparty(GruffSparty* gruffSparty)
+{
+    mSparties.push_back(gruffSparty);
+    mNumSparty++;
+}
--- SpartyLib/SpartyTracker.h
+++ SpartyLib/SpartyTracker.h
@@ -2,18 +2,37 @@
  * @file SpartyTracker.h
  * @author Ian Valdovinos Granados
  *
- *
+ * Concrete visitor that keeps track of the Angry Sparties
  */
 
 #ifndef ANGRYSPARTY_SPARTYTRACKER_H
 #define ANGRYSPARTY_SPARTYTRACKER_H
 
 #include "ItemVisitor.h"
+#include "Angry.h"
+#include "HelmetSparty.h"
+#include "GruffSparty.h"
 
+/**
+ * Concrete visitor that keeps track of the Angry Sparties
+ */
 class SpartyTracker : public ItemVisitor {
 private:
+    /// Angry Sparty counter
+    int mNumSparty = 0;
 
+    /// All of the Angry Sparties
+    std::vector<Angry *> mSparties;
 public:
+    /**
+     * Get the number of Angry Sparties
+     * @return Number of Angry Sparties
+     */
+    int GetNumSparties() const {return mNumSparty; }
+
+    void VisitHelmetSparty(HelmetSparty* helmetSparty) override;
+
+    void VisitGruffSparty(GruffSparty* gruffSparty) override;
 
 };
 
