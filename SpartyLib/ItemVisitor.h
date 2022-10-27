301f52415a845bce4136dc2ffcc7cd9abfa38b50
Ian Valdovinos <valdovi7@msu.edu>
2022-10-27 18:43:38 -0400

    Add and setup ItemVisitor base class.

---
+++ SpartyLib/ItemVisitor.h
@@ -0,0 +1,55 @@
+/**
+ * @file ItemVisitor.h
+ * @author Ian Valdovinos Granados
+ *
+ * Item visitor base class
+ */
+
+#ifndef ANGRYSPARTY_ITEMVISITOR_H
+#define ANGRYSPARTY_ITEMVISITOR_H
+
+// Forward references to all item types
+class Background;
+class Goalpost;
+class WoodenSlingshot;
+class Block;
+class Poly;
+class Foe;
+class GruffSparty;
+class HelmetSparty;
+
+/**
+ * Item visitor base class
+ */
+class ItemVisitor
+{
+protected:
+    /**
+     * Constructor
+     * Ensures this is an abstract class
+     */
+    ItemVisitor() {}
+
+public:
+
+    virtual ~ItemVisitor() {}
+
+    virtual void VisitBackground(Background* background) {}
+
+    virtual void VisitWoodenSlingshot(WoodenSlingshot* woodenSlingshot) {}
+
+    virtual void VisitGoadlpost(Goalpost* goalpost) {}
+
+    virtual void VisitBlock(Block* block) {}
+
+    virtual void VisitPoly(Poly* poly) {}
+
+    virtual void VisitFoe(Foe* foe) {}
+
+    virtual void VisitGruffSparty(GruffSparty* gruffSparty) {}
+
+    virtual void VisitHelmetSparty(HelmetSparty* helmetSparty) {}
+
+};
+
+#endif //ANGRYSPARTY_ITEMVISITOR_H
