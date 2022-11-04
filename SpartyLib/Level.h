72041c6d0802041e42b401e2de06c4d46665b410
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2022-11-03 22:52:06 -0400

    Level

--- SpartyLib/Level.h
+++ SpartyLib/Level.h
@@ -28,7 +28,10 @@ class Angry;
  */
 class Level {
 private:
+    /// Height of the level
     double mHeight = 0;
+
+    /// Width of the level
     double mWidth = 0;
 
     ///an pointer to sparty game
@@ -43,8 +46,6 @@ private:
     std::shared_ptr<SpartyTracker> mSpartyTracker;
 
 public:
-    double getHeight() { return mHeight; };
-    double getWidth() { return mWidth; };
 
     /**
      * Returns the position of the item
@@ -52,6 +53,7 @@ public:
      */
     b2Vec2 GetPosition() { return b2Vec2(mWidth, mHeight); }
 
+    /// Copy constructor (disabled)
     Level() = delete;
 
     Level(SpartyGame *spartyGame);
@@ -62,14 +64,28 @@ public:
 
     void SetSlingShot(std::shared_ptr<Slingshot> slingshot);
 
-    //Level(double pHeight, double pWidth) : mHeight(pHeight), mWidth(pWidth){};
+    /// function for initialization
     void init();
 
+    /**
+     * returns the Height of the level
+     * @return double representing the height of the level
+     */
     double GetHeight(){return mHeight;}
+
+    /**
+     * returns the Width of the level
+     * @return double representing the width of the levelx
+     */
     double GetWidth(){return mWidth;}
 
-    void LoadXMLLevel(wxXmlNode *node);
+    /// function Loading the XML Level
+//    void LoadXMLLevel(wxXmlNode *node);
 
+    /**
+     * returns the play are size as a b2Vec2 of the current level
+     * @return b2Vec2 of the current level
+     */
     b2Vec2 GetPlayAreaSize(){return b2Vec2(mWidth, mHeight);}
 
     void Load(wxXmlNode *node);
@@ -80,9 +96,22 @@ public:
 
     Angry* HitTest(int x, int y);
 
+    /**
+     * Returns the scale of the mSpartyGame
+     * @return double of the score
+     */
     double GetGameScale() {return mSpartyGame->GetScale();}
 
+    /**
+     * Returns the sparty games X offset
+     * @return double representing the X offset of the sparty game
+     */
     double GetGameXOffset() {return mSpartyGame->GetXOffset();}
+
+    /**
+     * Returns the sparty games Y offset
+     * @return double representing the Y offset of the sparty game
+     */
     double GetGameYOffset() {return mSpartyGame->GetYOffset();}
 
     void DrawRightSlingshotRubberBand(std::shared_ptr<wxGraphicsContext> graphics);
